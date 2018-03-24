609ac2d9d97124797d63571077d884942f61959c
HimaVarsha94 <cs12b1042@iith.ac.in>
2018-03-23 22:54:44 -0400

    added a file for senna embeddings

---
+++ extract_senna.py
@@ -0,0 +1,27 @@
+import pickle
+import numpy as np
+
+def senna():
+	emb = open('./senna_embeddings/embeddings.txt')
+	words = open('./senna_embeddings/words.lst')
+	senna_dict = {}
+	senna_words_list = []
+
+	for word in words:
+		senna_words_list.append(word.strip())
+	a = []
+	ind = 0
+	for line in emb:
+		nums = line.strip().split(' ')
+		nums = np.array(nums)
+		senna_dict[senna_words_list[ind]] = nums
+		ind += 1
+	with open( 'senna_obj.pkl', 'w') as f:
+		pickle.dump(senna_dict, f)
+
+def main():
+	senna()
+	return
+
+if __name__ == '__main__':
+	main()

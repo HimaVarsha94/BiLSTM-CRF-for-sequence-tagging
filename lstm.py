25f727404d9da6d7f3ba3fd8d56489f555da8038
HimaVarsha94 <cs12b1042@iith.ac.in>
2018-03-28 21:34:53 -0400

    bug fix

--- lstm.py
+++ lstm.py
@@ -9,7 +9,7 @@ torch.manual_seed(1)
 class LSTMTagger(nn.Module):
 
     def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size, pretrained_weight_embeddings, USE_CRF=False,
-                 BIDIRECTIONAL=False, USE_BIGRAM=False, bigram_size=0):
+                 BIDIRECTIONAL=False, USE_BIGRAM=False, bigram_size=0, CNN=False):
         super(LSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
@@ -17,8 +17,10 @@ class LSTMTagger(nn.Module):
         print("Entered!!!!")
         # if pretrained_weight_embeddings != None:
         self.word_embeddings.weight.data.copy_(torch.from_numpy(pretrained_weight_embeddings))
-
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2, bidirectional=BIDIRECTIONAL)
+        if BIDIRECTIONAL:
+            self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2, bidirectional=BIDIRECTIONAL)
+        else:
+            self.lstm = nn.LSTM(embedding_dim, hidden_dim)
 
         self.bidirectional = BIDIRECTIONAL
         self.append_bigram = USE_BIGRAM

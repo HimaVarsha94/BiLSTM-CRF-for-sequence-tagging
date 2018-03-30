30591b400944cf3f8be343433202d696562ae7ac
George Larionov <georgelarionov@gmail.com>
2018-03-30 00:21:51 -0400

    added viterbi and finished up crf impl

--- models/crf.py
+++ models/crf.py
@@ -8,6 +8,11 @@ START_TAG = -1
 END_TAG = -2
 MIN_VAL = -100000
 
+## accepts torch.Tensor of size 1xN and returns log sum exp
+def lsm(vec):
+    max_val = torch.max(vec)
+    return torch.log(torch.sum(torch.exp(vec - max_val))) + max_val
+
 class CRF(nn.Module):
 
     def __init__(self, num_tags, gpu):
@@ -47,8 +52,44 @@ class CRF(nn.Module):
         final_alpha = alpha + self.trans[END_TAG]
         return lsm(final_alpha)
 
-    def viterbi(self, feats):
-        pass
+    def viterbi(self, feats, sent_len):
+        ## init table of probs of most likely path so far
+        init_v_probs = torch.Tensor(1,self.num_tags+2).fill_(MIN_VAL)
+        init_v_probs[0,START_TAG] = 0
+        vprobs = autograd.Variable(init_v_probs)
+        if gpu:
+            vprobs = vprobs.cuda()
+        ## init table of backpointers, same size
+        backptrs = []
+
+        ## fill out table
+        for feat in feats:
+            inner_vprobs = torch.zeros(self.num_tags+2)
+            inner_backptrs = torch.zeros(self.num_tags+2)
+            for to_tag in range(self.num_tags+2):
+                ## calc probs for transition to to_tag
+                ## add in emission probs later (they don't depend on to_tag)
+                temp_probs = self.trans[to_tag].view(1,-1) + vprobs
+                ## this is best last tag
+                best_tag = torch.max(temp_probs)
+                inner_backptrs[to_tag] = best_tag
+                inner_vprobs[to_tag] = temp_probs[0,best_tag]
+            backptrs.append(inner_backptrs)
+            vprobs = (inner_vprobs + feat).view(1,-1)
+
+        ## transition to END_TAG
+        temp_probs = self.trans[END_TAG].view(1,-1) + vprobs
+        final_tag = torch.max(temp_probs)
+        final_score = temp_probs[0,best_tag]
+
+        ## follow backpointers to get best sequence
+        best_seq = [final_tag]
+        for backptr_vec in reversed(backptrs):
+            best_seq.append(backptr_vec[best_seq[-1]])
+        ## don't want to return START_TAG
+        best_seq.pop().reverse()
+
+        return final_score, best_seq
 
     def get_gold_scores(self, feats, tags):
         score = autograd.Variable(torch.Tensor([0]))
@@ -65,5 +106,5 @@ class CRF(nn.Module):
         return forward_scores - gold_scores
 
     def forward(self, sentence, feats):
-        score, tag_seq = viterbi(feats)
+        score, tag_seq = viterbi(feats, len(sentence))
         return score, tag_seq

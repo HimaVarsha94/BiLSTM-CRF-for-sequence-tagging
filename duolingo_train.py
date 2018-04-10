c911748c9dfce133608a5f5a195d20f0763910d1
HimaVarsha94 <cs12b1042@iith.ac.in>
2018-04-10 00:53:07 -0400

    changes to test it on BLSTM+CNNs+CRF

--- duolingo_train.py
+++ duolingo_train.py
@@ -1,12 +1,27 @@
+import time, os
 import numpy as np, pickle
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from models.lstm import LSTMTagger
-# import baseline
-torch.manual_seed(1)
+# from models.lstm import LSTMTagger
+from models.lstm_cnn import BILSTM_CNN
+# from models.bilstm_crf_cnn import BiLSTM_CRF_CNN
+from sklearn.metrics import f1_score, precision_score, recall_score, roc_curve
+from random import shuffle
+#torch.cuda.set_device(3)
+# torch.manual_seed(1)
+
+def cap_feature(s):
+    if s.lower() == s:
+        return 0
+    elif s.upper() == s:
+        return 1
+    elif s[0].upper() == s[0]:
+        return 2
+    else:
+        return 3
 
 def get_embeddings_matrix(data):
     word_to_ix = {'unk':0}
@@ -16,11 +31,11 @@ def get_embeddings_matrix(data):
             if word not in word_to_ix:
                 word_to_ix[word] = len(word_to_ix)
 
-    with open('./chunking_models/word_to_ix.pkl', 'wb') as f:
+    with open('./models/duolingo_models/word_to_ix.pkl', 'wb') as f:
         pickle.dump(word_to_ix, f)
         
     #this contains a list of all senna obj embeddings
-    with open('./senna_embeddings/senna_obj.pkl', 'rb') as f:
+    with open('./embeddings/senna_obj.pkl', 'rb') as f:
         senna_obj = pickle.load(f)
     #embeddings matrix with each row corresponding to a word to pass to nn.embedding layer
     embeddings_mat = np.zeros((len(word_to_ix), 50))
@@ -31,138 +46,306 @@ def get_embeddings_matrix(data):
         else:
             embeddings_mat[word_to_ix[word]] = np.zeros(50)
 
-    with open('./chunking_models/conll2000_matrix', 'wb') as f:
+    with open('./models/duolingo_models/duolingo_matrix', 'wb') as f:
         pickle.dump(embeddings_mat, f)
 
     return embeddings_mat, word_to_ix
 
+def get_glove_matrix(data):
+    word_to_ix = {'unk':0}
+    for sent in data:
+        for word in sent:
+            word = word.lower()
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+
+    with open('./models/duolingo_models/word_to_ix.pkl', 'wb') as f:
+        pickle.dump(word_to_ix, f)
+    #this contains a list of all senna obj embeddings
+    with open('./embeddings/glove_obj.pkl', 'rb') as f:
+        glove_obj = pickle.load(f)
+    #embeddings matrix with each row corresponding to a word to pass to nn.embedding layer
+    embeddings_mat = np.zeros((len(word_to_ix), 100))
+
+    for word in word_to_ix:
+        if word in glove_obj:
+            embeddings_mat[word_to_ix[word]] = glove_obj[word]
+        else:
+            embeddings_mat[word_to_ix[word]] = np.random.rand(100)
+
+    with open('./models/duolingo_models/duolingo_matrix', 'wb') as f:
+        pickle.dump(embeddings_mat, f)
+
+    return embeddings_mat, word_to_ix
 
 def prepare_sequence(seq, to_ix, tag=False):
     idxs = []
     for w in seq:
-        if tag==False:
+        if tag == False:
             w = w.lower()
         if w in to_ix:
             idxs.append(to_ix[w])
         else:
             idxs.append(0)
     tensor = torch.LongTensor(idxs)
-    return autograd.Variable(tensor)
-
-def chunking_preprocess(datafile, senna=True):
-    counter = 0
-    data = []
-    X = []
-    y = []
-    new_data = []
-    new_label = []
-    for line in datafile:
-        counter += 1
-        line = line.strip()
-        tokens = line.split(' ')
-        if len(tokens) == 1: 
-            X.append(new_data)
-            y.append(new_label)
-            new_data = []
-            new_label = []
-        else:
-            new_data.append(tokens[0])
-            new_label.append(tokens[2])
-    return X, y
-
-def load_duolingo(train=False, test=False):
-    with open('./data/duolingo/data.txt', 'rb') as f:
-        all_data = pickle.load(f)
-        print("all_data_length "+str(len(all_data)))
-    with open('./data/duolingo/data_labels.txt', 'rb') as f:
-        all_data_labels = pickle.load(f)
-    length = len(all_data)
-    split = int(length*0.9)
-    if train == True:
-        return all_data[:split], all_data_labels[:split]
-    if test == True:
-        pickle.dump(all_data[split:],open("./data/duolingo/test_data.txt","wb"))
-        pickle.dump(all_data_labels[split:], open("./data/duolingo/test_data_label.txt", "wb"))
-        print("testing data length is "+str(len(all_data_labels[split:])))
-        return all_data[split:], all_data_labels[split:]
-    import pdb; pdb.set_trace()
+    if use_gpu:
+        return autograd.Variable(tensor).cuda()
+    else:
+        return autograd.Variable(tensor)
 
 def tag_indices(X, y):
-    tag_to_idx = {}
+    tag_to_idx = {START_TAG: 0, END_TAG: 1}
+    idx_to_tag = {0: START_TAG, 1: END_TAG}
     for sent_tag in y:
         for tag in sent_tag:
             if tag not in tag_to_idx:
+                idx_to_tag[len(tag_to_idx)] = tag
                 tag_to_idx[tag] = len(tag_to_idx)
 
-    with open('./chunking_models/tag_to_idx', 'wb') as f:
+    with open('./models/pos_models/tag_to_idx', 'wb') as f:
         pickle.dump(tag_to_idx, f)
-    return tag_to_idx
+    return tag_to_idx, idx_to_tag
+
+def char_dict(data):
+    char_to_ix = {}
+    for sent in data:
+        for word in sent:
+            word = word.lower()
+            for character in word:
+                if character not in char_to_ix:
+                    char_to_ix[character] = len(char_to_ix)
+
+    with open('./models/pos_models/char_to_ix.pkl', 'wb') as f:
+        pickle.dump(char_to_ix, f)
+
+    return char_to_ix
+
+def prepare_words(sentence, char_to_ix):
+    d = []
+    for w in sentence:
+        w = w.lower()
+        idxs = []
+        for char in w:
+            if char in char_to_ix:
+                idxs.append(char_to_ix[char])
+            else:
+                idxs.append(0)
+        d.append(idxs)
+    return d
+    # tensor = torch.LongTensor(d)
+    # return autograd.Variable(tensor)
+
+def char_emb(chars2):
+    if len(chars2) == 0:
+        print("Oh")
+        return
+    chars2_length = [len(c) for c in chars2]
+    char_maxl = max(chars2_length)
+    chars2_mask = np.zeros((len(chars2_length), char_maxl), dtype='int')
+    for i, c in enumerate(chars2):
+        chars2_mask[i, :chars2_length[i]] = c
+    if use_gpu:
+        chars2_mask = autograd.Variable(torch.cuda.LongTensor(chars2_mask))
+    else:
+        chars2_mask = autograd.Variable(torch.LongTensor(chars2_mask))
+    return chars2_mask
+
+def get_results(filename, model, data_X, data_Y, epoch, idx_to_tag, word_to_ix, tag_to_ix, char_to_ix, CNN, use_gpu):
+    correct = 0
+    total = 0
+    all_predicted = []
+    all_targets= []
+
+    len_test = len(data_X)
+    print("Testing length", len_test)
+    fname = filename+ str(epoch)+'.txt'
+    with open(fname,'w') as f:
+        for ind in range(len_test):
+            sentence = data_X[ind]
+            tags = data_Y[ind]
+
+            sentence_in = prepare_sequence(sentence, word_to_ix)
+            targets = prepare_sequence(tags, tag_to_ix, tag=True)
+            if use_gpu:
+                caps = autograd.Variable(torch.LongTensor([cap_feature(w) for w in sentence])).cuda()
+            else:
+                caps = autograd.Variable(torch.LongTensor([cap_feature(w) for w in sentence]))
+            if CNN:
+                char_in = prepare_words(sentence, char_to_ix)
+                char_em = char_emb(char_in)
+                prob, tag_seq = model(sentence_in, char_em, caps, 0)
+                predicted = torch.LongTensor(tag_seq)
+            else:
+                tag_scores = model(sentence_in)
+                prob, predicted = torch.max(tag_scores.data, 1)
+
+            if use_gpu:
+                all_predicted = all_predicted + (autograd.Variable(predicted).data.cpu().numpy().tolist())
+                all_targets = all_targets + (targets.data.cpu().numpy().tolist())
+            else:
+                all_predicted.append(predicted)
+                all_targets.append(targets)
+            if use_gpu:
+                correct += (predicted.cuda() == targets.data).sum()
+            else:
+                correct += (predicted == targets.data).sum()
+
+            total += targets.size(0)
+            for tag_ in range(len(sentence)):
+                f.write(sentence[tag_] + " " + tags[tag_]+" "+idx_to_tag[predicted[tag_]]+"\n")
+            f.write("\n")
+    print("F1 score is ", f1_score(all_targets, all_predicted))
+    fpr, tpr, thresholds = roc_curve(all_targets, all_predicted, pos_label=1)
+    print("ROC values ", fpr, tpr)
+    print("Accuracy is ", float(correct)/total)
+
+
+def load_duolingo(train=False, test=False):
+
+    if train == True:
+        with open('./data/duolingo/train_data.txt', 'rb') as f:
+            all_data = pickle.load(f)
+        print("all_data_length "+str(len(all_data)))
+        with open('./data/duolingo/train_data_labels.txt', 'rb') as f:
+            all_data_labels = pickle.load(f)
+        return all_data, all_data_labels
+
+    if test == True:
+        with open('./data/duolingo/test_data.txt', 'rb') as f:
+            all_data = pickle.load(f)
+        print("all_data_length "+str(len(all_data)))
+        with open('./data/duolingo/test_data_labels.txt', 'rb') as f:
+            all_data_labels = pickle.load(f)
+        return all_data, all_data_labels
+        
+    import pdb; pdb.set_trace()
+
+
+def adjust_learning_rate(optimizer, epoch, LR):
+    lr = LR / (1 + 0.5*epoch)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
 def main():
+
+    USE_CRF = False
+    HIDDEN_DIM = 200
+    CNN = True
+    SENNA = False
+    BIDIRECTIONAL = True
+    bilstm_crf_cnn_flag = False
+
     training_data, y = load_duolingo(train=True)
-    from chunking_train import avg_len
-    print("average len is " + str(avg_len(training_data)))
     test_X, test_y = load_duolingo(test=True)
-    emb_mat, word_to_ix = get_embeddings_matrix(training_data)
-    tag_to_ix = tag_indices(training_data, y) 
-    
+    dev_X, dev_y = load_duolingo(dev=True)
 
-    EMBEDDING_DIM = 50
-    HIDDEN_DIM = 300
+    char_to_ix = char_dict(training_data)
+    tag_to_ix, idx_to_tag = tag_indices(training_data, y)
 
-    model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix), emb_mat)
-    loss_function = nn.NLLLoss()
+    if SENNA:
+        EMBEDDING_DIM = 50
+        emb_mat, word_to_ix = get_embeddings_matrix(training_data)
+    else:
+        print("Glove")
+        EMBEDDING_DIM = 100
+        emb_mat, word_to_ix = get_glove_matrix(training_data)
+
+    if CNN == False and USE_CRF == False:
+        print("Bilstm")
+        model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))#, emb_mat, USE_CRF, BIDIRECTIONAL=True)
+    if CNN == True and USE_CRF == False:
+        print("Using BiLSTM-CNN-CRF")
+        bilstm_crf_cnn_flag = True
+        model = BILSTM_CNN(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix), len(char_to_ix), emb_mat, tag_to_ix, CNN=True, BIDIRECTIONAL=True, use_gpu=use_gpu)
+    if CNN == True and USE_CRF == True:
+        bilstm_crf_cnn_flag = True
+        print('Using BiLSTM-CRF-CNN')
+        model = BiLSTM_CRF_CNN(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix), len(char_to_ix), emb_mat, tag_to_ix, USE_CRF, BIDIRECTIONAL, False, 0, CNN, use_gpu)
+
+    if use_gpu:
+        model = model.cuda()
+
+    if not bilstm_crf_cnn_flag:
+        loss_function = nn.NLLLoss()
     parameters = model.parameters()
     # parameters = filter(lambda p: model.requires_grad, model.parameters())
-    optimizer = optim.SGD(parameters, lr=0.1)
+    # optimizer = optim.Adam(parameters, lr=0.001)
+    learning_rate = 0.01
+    optimizer = optim.SGD(parameters, lr=learning_rate)
 
     len_train = len(training_data)
     len_test = len(test_X)
-    for epoch in range(50):
+    print("Number of train sentences ", len_train)
+    print("Number of test sentences ", len_test)
+    print('Training...')
+
+    indices =[i for i in range(len_train)]
+    shuffle(indices)
+    last_time = time.time()
+    fake_count = 0
+    for epoch in range(500):
         loss_cal = 0.0
-        print(epoch)
-        for ind in range(int(len_train/100)):
-            if ind%1000==0:
-                print(ind)
+        count = 0
+        for ind in (indices):
+            count += 1
             sentence = training_data[ind]
             tags = y[ind]
+            # data = all_data[ind]
             model.zero_grad()
-            model.hidden = model.init_hidden()
+            # THIS NOW HAPPENS IN FORWARD
+            # model.hidden = model.init_hidden()#########
             sentence_in = prepare_sequence(sentence, word_to_ix)
-            targets = prepare_sequence(tags, tag_to_ix, tag=True)
-            tag_scores = model(sentence_in)
-            loss = loss_function(tag_scores, targets)
-            loss_cal += loss
-            loss.backward()
+            # sentence_in = data['sent']
+            # targets = prepare_sequence(tags, tag_to_ix, tag=True)########
+            # targets = data['tags']
+            caps = autograd.Variable(torch.LongTensor([cap_feature(w) for w in sentence]))
+            targets = torch.LongTensor([tag_to_ix[t] for t in tags])
+
+            # targets = data['tags']
+            # import pdb; pdb.set_trace()
+            if bilstm_crf_cnn_flag:
+                char_in = prepare_words(sentence, char_to_ix)
+                # char_in = data['chars']
+                char_em = char_emb(char_in)
+                # import pdb; pdb.set_trace()
+                if use_gpu:
+                    nll = model.neg_ll_loss(sentence_in.cuda(), targets, char_em.cuda(), caps.cuda(), 0.5)
+                else:
+                    nll = model.neg_ll_loss(sentence_in, targets, char_em, caps, 0.5)
+                loss_cal += nll
+                nll.backward()
+                torch.nn.utils.clip_grad_norm(model.parameters(), 5.0)
+            elif CNN:
+                char_in = prepare_words(sentence, char_to_ix)
+                char_em = char_emb(char_in)
+                tag_scores = model(sentence_in,char_em)
+            else:
+                tag_scores = model(sentence_in)
+
+            if not bilstm_crf_cnn_flag:
+                loss = loss_function(tag_scores, targets)
+                loss_cal += loss
+                loss.backward()
 
             optimizer.step()
-        PATH = './duolingo_models/model_epoch'+str(epoch)
-        torch.save(model.state_dict(), PATH)
-        model.load_state_dict(torch.load(PATH))
+            if count % 1000 == 0 and ((count > 20000 and epoch==0) or (epoch!=0)):
+                print(loss_cal)
+                loss_cal = 0
+                print(count, epoch)
+                if SENNA:
+                    # get_results('text/train_ner_bilstm_cnn', model, training_data, y, epoch, idx_to_tag, word_to_ix, tag_to_ix, char_to_ix, CNN, use_gpu)
+                    get_results('text/test_ner_bilstm_cnn', model, test_X, test_y, epoch, idx_to_tag, word_to_ix, tag_to_ix, char_to_ix, CNN, use_gpu)
+                else:
+                    # get_results('pos_glove_text/train_ner_bilstm_cnn', model, training_data, y, ind, idx_to_tag, word_to_ix, tag_to_ix,char_to_ix, CNN, use_gpu)
+                    # get_results('pos_glove_text/dev_ner_bilstm_cnn', model, dev_X, dev_y, ind, idx_to_tag, word_to_ix, tag_to_ix, char_to_ix, CNN, use_gpu)
+                    get_results('pos_glove_text/test_ner_bilstm_cnn', model, test_X, test_y, ind, idx_to_tag, word_to_ix, tag_to_ix, char_to_ix, CNN, use_gpu)
+            if count == 0 or count == 20000:
+                fake_count += 1
+                print("Annealing")
+                adjust_learning_rate(optimizer, fake_count, learning_rate)
 
-        print(loss_cal)
-        print("Testing!!")
-        correct = 0
-        total = 0
-        predicted_values = []
-        print(len_test)
-        for ind in range(len_test):
-            if ind%5000==0:
-                print(ind)
-            sentence = test_X[ind]
-            tags = test_y[ind]
-            sentence_in = prepare_sequence(sentence, word_to_ix)
-            targets = prepare_sequence(tags, tag_to_ix, tag=True)
-            tag_scores = model(sentence_in)
-            prob, predicted = torch.max(tag_scores.data, 1)
-            predicted_values.append(predicted)
-            correct += (predicted == targets.data).sum()
-            total += targets.size(0)
-            loss = loss_function(tag_scores, targets)
-        print("Accuracy of epoch {} is {}".format(epoch, float(correct)/total))
-        with open('./duolingo_models/results'+str(epoch), 'wb') as f:
-            pickle.dump(predicted_values, f)
-
+        print('Epoch {} took {:.3f}s'.format(epoch,time.time() - last_time))
+        last_time = time.time()
 
 if __name__ == '__main__':
     main()
